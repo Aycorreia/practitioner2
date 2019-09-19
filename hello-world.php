725cc3bfaabe25dfbe1d9b98a0d50b4ef9ea159a
Aycorreia <ayrampenalver@iesebre.com>
2019-09-19 20:56:51 +0200

    WIP

--- hello-world.php
+++ hello-world.php
@@ -1,8 +1,19 @@
 <?php
 $greeting = "Hola";
+$rre = 'Hola';
+
 
 //aixo es un comentari
 // ja no es fa $stringGreetingadszf
 //$user =
+//$name = 'Ayr'
+//si es vol barrejar php amb html, es te que guardar en php.
+//oco javascript es +
 
-echo $greeting;
+//concatenar strings. en javascript es (+)!!!
+echo 'Hola ' .$greeting;
+
+echo 'Hola $greeting';
+echo "Hola $greeting";
+
+echo "Hola ($greeting) que tal";
