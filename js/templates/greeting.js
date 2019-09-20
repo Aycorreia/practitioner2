288b096f6ee8365f4ae16e9f3b6c4e65400109a6
Aycorreia <ayrampenalver@iesebre.com>
2019-09-20 17:03:30 +0200

    WIP

--- js/templates/greeting.js
+++ js/templates/greeting.js
@@ -1,4 +1,10 @@
-//aqui la plantilla
-const template = `<h1>${name}!</h1>`
+"use strict";
 
-export default template
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+//aqui la plantilla
+var template = "<h1>".concat(name, "!</h1>");
+var _default = template;
+exports["default"] = _default;
--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
-"use strict";
-
-var _greeting = _interopRequireDefault(require(".js/templates/greeting.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
 //console.log('Hola mon')
+
 //chibatar lo que fa aquesta cosa!
 //document lo que fa es carregar el document html per a modificarlo en js?
 console.log(document);
-console.log(document.body); //carrega la pagina i inserta la linea!
+
+console.log(document.body);
+
+//carrega la pagina i inserta la linea!
 //aixo es obsolet sin embargo.
 ///document.body.innerHTML ='<h1> Heyyy!!! </h1>'
 //// Aixo carrega lo H1 i lo reemplaca!
@@ -16,24 +14,44 @@ console.log(document.body); //carrega la pagina i inserta la linea!
 //el resultat a la vista. El two way, segons he entes, es que el javascript carregue el codi automaticament
 // a la vista mentre el usuari lo usa. No se.
 
-var name = 'Ayr'; //claudators!! no parenteisis
+const name = 'Ayr'
+
+//claudators!! no parenteisis
 //const template = `<h1>${name}!</h1>`
 //importem aixo desde altre fitxer.
+
 //es separen les plantilles.
 //control shift 7 per a comentar normal. A per a comentar en bloc
 //cntr + e per a buscar archius i fitxers al code.
-//per a indicar moduls propis cal fer / per a tot el path, o . per a relatius.
 
-console.log(_greeting["default"]);
-document.body.innerHTML = template; //gestor de depencencies! sino cal importarles tot al document...
+//per a indicar moduls propis cal fer / per a tot el path, o . per a relatius.
+import greeting from '.js/templates/greeting.js'
+
+console.log(greeting);
+
+
+document.body.innerHTML = template
+
+//gestor de depencencies! sino cal importarles tot al document...
+
 //npm es un gestor. tambe esta yarn.
 //si es vol usar bable, can instalar una dependencia..-
+
 //nodemodules (js) no es toca mai sense saber lo que es fa! vendor per a php.
 // son les dependencies!
 // sol si peta el programa de altre, si lo teu el peta.
 // pero es pot borrar. sempre que es pugi fer el npm per instalarlo altra vegada.
 //php es composer!
 //pacaje-loc tambe lo mateix.
+
 //manifest definira el nom i dependencies del projecte.
+
+
 // se han de definir presets al babel.
+
+//cal instalar webpack!
+
 //import i export
+
+
+
